b2cf67b374254102fbbb7c3118b649c2df5309bb
dudethat1 <47578196+dudethat1@users.noreply.github.com>
2019-03-13 13:17:48 -0400

    changed from keyboard to ascii

--- GameTime/Assets/Scripts/Instrument_Controller.cs
+++ GameTime/Assets/Scripts/Instrument_Controller.cs
@@ -32,12 +32,12 @@ public class Instrument_Controller : MonoBehaviour
 		float lerp_factor = current_instrument.current_time - current_instrument.initial_time;
 		
 		//speed
-        if (Input.GetKeyDown("w")){
-		//if (input_variable == 17){
+        //if (Input.GetKeyDown("w")){
+		if (input_variable == 17){
 			current_instrument.travel_time += 0.05f;
 		}
-		if (Input.GetKeyDown("s")){
-		//if (input_variable == 31){
+		//if (Input.GetKeyDown("s")){
+		if (input_variable == 31){
 			current_instrument.travel_time -= 0.05f;
 			if (current_instrument.travel_time <= 0.1f){
 				current_instrument.travel_time = 0.1f;
@@ -45,22 +45,22 @@ public class Instrument_Controller : MonoBehaviour
 		}
 		
 		//offset
-		if (Input.GetKeyDown("d")){
-		//if (input_variable == 32){
+		//if (Input.GetKeyDown("d")){
+		if (input_variable == 32){
 			if (lerp_factor < current_instrument.travel_time-offset_factor){
 				current_instrument.current_time += offset_factor;
 			}
 		}
-		if (Input.GetKeyDown("a")){
-		//if (input_variable == 30){
+		//if (Input.GetKeyDown("a")){
+		if (input_variable == 30){
 			if (lerp_factor > offset_factor){
 				current_instrument.current_time -= offset_factor;
 			}
 		}
 		
 		//choose instrument
-		if (Input.GetKeyDown("up")){
-		//if (input_variable == 103){
+		//if (Input.GetKeyDown("up")){
+		if (input_variable == 103){
 			instruments[index].GetComponent<Renderer>().material = temp_material;
 			if (index == 2){
 				index = 0;
@@ -79,8 +79,8 @@ public class Instrument_Controller : MonoBehaviour
 			indicator.transform.position = indicator_locations[index].transform.position;
 		}
 		
-		if (Input.GetKeyDown("down")){
-		//if (input_variable == 108){
+		//if (Input.GetKeyDown("down")){
+		if (input_variable == 108){
 			instruments[index].GetComponent<Renderer>().material = temp_material;
 			if (index == 0){
 				index = 2;
@@ -97,8 +97,8 @@ public class Instrument_Controller : MonoBehaviour
 			indicator.transform.position = indicator_locations[index].transform.position;
 		}
 		
-		if (Input.GetKeyDown("g")){
-		//if (input_variable == 34){
+		//if (Input.GetKeyDown("g")){
+		if (input_variable == 34){
 			//instruments[index].SetActive(!instruments[index].activeInHierarchy);
 			if (instruments[index].transform.localScale.x == 0){
 				instruments[index].transform.localScale =  new Vector3(1,1,1);
@@ -107,32 +107,32 @@ public class Instrument_Controller : MonoBehaviour
 			}
 		}
 		
-		if (Input.GetKeyDown("r")){
-		//if (input_variable == 19){
+		//if (Input.GetKeyDown("r")){
+		if (input_variable == 19){
 			foreach (GameObject i in instruments){
 				Metronome temp = i.GetComponent<Metronome>();
 				temp.current_time = temp.initial_time+0.5f*temp.travel_time;
 			}
 		}
 		
-		if (Input.GetKeyDown("j")){
-		//if (input_variable == 36){
+		//if (Input.GetKeyDown("j")){
+		if (input_variable == 36){
 			foreach (GameObject i in instruments){
 				Metronome temp = i.GetComponent<Metronome>();
 				temp.moving = !temp.moving;
 			}
 		}
 		
-		if (Input.GetKeyDown("h")){
-		//if (input_variable == 35){
+		//if (Input.GetKeyDown("h")){
+		if (input_variable == 35){
 			foreach (GameObject i in instruments){
 				Metronome temp = i.GetComponent<Metronome>();
 				temp.travel_time = 1;
 			}
 		}
 		
-		if (Input.GetKeyDown("o")){
-		//if (input_variable == 24){
+		//if (Input.GetKeyDown("o")){
+		if (input_variable == 24){
 			foreach (GameObject i in instruments){
 				Metronome temp = i.GetComponent<Metronome>();
 				temp.travel_time = 1;
